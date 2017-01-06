f66f92708de1807b800d8b3f21d131f914bd3f0d
Þorvaldur Rúnarsson <valdirunars@gmail.com>
2017-01-06 02:41:26 +0000

    updated kitura version to 1.3

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
       Target(name: "SimpleOAuth2", dependencies: [])
     ],
     dependencies: [
-      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 2),
+      .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 3),
       .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 8),
       .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
       .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 6),
